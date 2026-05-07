f4d36f4cf49f964ef0d962beb624b20ea1735500
Samuel Mensah <samuel.mensah@student.ap.be>
2026-05-07 15:38:24 +0200

    horizontale scroll gefixed

--- js/kaart.js
+++ js/kaart.js
@@ -1,7 +1,7 @@
-var map = L.map('kaart').setView([51.241753720840734, 4.4451214488863515], 13);
+var map = L.map('kaart').setView([51.24136515573733, 4.445437650684831], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.241753720840734, 4.4451214488863515]).addTo(map);
+L.marker([51.24136515573733, 4.445437650684831]).addTo(map);
